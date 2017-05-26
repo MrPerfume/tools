7acb8c14c234240f87ae7a552d8517f9a1e4e7cc
MrHuang <hoangluckyboy@icloud.com>
2017-05-26 23:38:28 +0800

    切割字符

--- helpers.class.php
+++ helpers.class.php
@@ -23,6 +23,36 @@ public static function microtime_float() {
 list ( $usec, $sec ) = explode ( " ", microtime () );
 return (( float ) $usec + ( float ) $sec);
 }
+/**
+* 切割utf-8格式的字符串(一个汉字或者字符占一个字节)
+*
+* @author zhao jinhan
+* @version v1.0.0
+*
+*/
+public static function truncate_utf8_string($string, $length, $etc = '...') {
+$result = '';
+$string = html_entity_decode ( trim ( strip_tags ( $string ) ), ENT_QUOTES, 'UTF-8' );
+$strlen = strlen ( $string );
+for($i = 0; (($i < $strlen) && ($length > 0)); $i ++) {
+if ($number = strpos ( str_pad ( decbin ( ord ( substr ( $string, $i, 1 ) ) ), 8, '0', STR_PAD_LEFT ), '0' )) {
+if ($length < 1.0) {
+break;
+}
+$result .= substr ( $string, $i, $number );
+$length -= 1.0;
+$i += $number - 1;
+} else {
+$result .= substr ( $string, $i, 1 );
+$length -= 0.5;
+}
+}
+$result = htmlspecialchars ( $result, ENT_QUOTES, 'UTF-8' );
+if ($i < $strlen) {
+$result .= $etc;
+}
+return $result;
+}
 
 
 
