09c9cd9871ae1521940e7db0e2b566fa0c48b499
MrHuang <hoangluckyboy@icloud.com>
2017-05-27 23:36:51 +0800

    判断文件是否可写

--- helpers.class.php
+++ helpers.class.php
@@ -229,6 +229,32 @@ $ret[$k] = $v[$key];
 }
 return $ret;
 }
+/**
+* 判断 文件/目录 是否可写（取代系统自带的 is_writeable 函数）
+* @param string $file 文件/目录
+* @return boolean
+*/
+public static function is_writeable($file) {
+if (is_dir($file)){
+$dir = $file;
+if ($fp = @fopen("$dir/test.txt", 'w')) {
+@fclose($fp);
+@unlink("$dir/test.txt");
+$writeable = 1;
+} else {
+$writeable = 0;
+}
+} else {
+if ($fp = @fopen($file, 'a+')) {
+@fclose($fp);
+$writeable = 1;
+} else {
+$writeable = 0;
+}
+}
+
+return $writeable;
+}
 
 
 
