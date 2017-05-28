8b4a5ab736b792e7c2d569df8d34c2efd76cb211
MrHuang <hoangluckyboy@icloud.com>
2017-05-28 23:49:54 +0800

    删除目录所有文件

--- helpers.class.php
+++ helpers.class.php
@@ -452,6 +452,31 @@ closedir($dir_p);
 }
 return $files;
 }
+/**
+* 删除文件夹及其文件夹下所有文件
+*/
+public static function deldir($dir) {
+//先删除目录下的文件：
+$dh=opendir($dir);
+while ($file=readdir($dh)) {
+if($file!="." && $file!="..") {
+$fullpath=$dir."/".$file;
+if(!is_dir($fullpath)) {
+unlink($fullpath);
+} else {
+self::deldir($fullpath);
+}
+}
+}
+
+closedir($dh);
+//删除当前文件夹：
+if(rmdir($dir)) {
+return true;
+} else {
+return false;
+}
+}
 
 
 
