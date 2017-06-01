fa470a23dde330291ffa02ace963c71673d9e648
MrHuang <hoangluckyboy@icloud.com>
2017-06-01 22:11:22 +0800

    下载文件

--- helpers.class.php
+++ helpers.class.php
@@ -771,7 +771,34 @@ if (file_exists($filename)) unlink($filename);
 static public function delDir($path) {
 if (is_dir($path)) rmdir($path);
 }
-
+/**
+* 下载文件
+* @param string $file_path 绝对路径
+*/
+static public function downFile($file_path) {
+//判断文件是否存在
+$file_path = iconv('utf-8', 'gb2312', $file_path); //对可能出现的中文名称进行转码
+if (!file_exists($file_path)) {
+exit('文件不存在！');
+}
+$file_name = basename($file_path); //获取文件名称
+$file_size = filesize($file_path); //获取文件大小
+$fp = fopen($file_path, 'r'); //以只读的方式打开文件
+header("Content-type: application/octet-stream");
+header("Accept-Ranges: bytes");
+header("Accept-Length: {$file_size}");
+header("Content-Disposition: attachment;filename={$file_name}");
+$buffer = 1024;
+$file_count = 0;
+//判断文件是否结束
+while (!feof($fp) && ($file_size-$file_count>0)) {
+$file_data = fread($fp, $buffer);
+$file_count += $buffer;
+echo $file_data;
+}
+fclose($fp); //关闭文件
+}
+}
 
 
 
